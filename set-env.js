a6825aa8bd9ed86d8152b4ec3b5c2f9943dddb9b
Juanqu <juanqu288@tabiconnect.com>
2026-03-08 15:03:53 -0500

    udpate: env load

--- set-env.js
+++ set-env.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 
 const targetPath = './src/environments/environment.prod.ts';
 
-// Extract the environment variable (Netlify will provide this during the 'npm run build' step)
 // We provide a fallback just in case it is run without the variable explicitly set
 const apiUrl = process.env.API_URL || 'https://YOUR_BACKEND_DOMAIN.com/api';
 
@@ -13,7 +12,6 @@ const envConfigFile = `export const environment = {
 };
 `;
 
-console.log(`Generating environment.prod.ts with API_URL: ${apiUrl}`);
 
 fs.writeFile(targetPath, envConfigFile, function (err) {
     if (err) {
